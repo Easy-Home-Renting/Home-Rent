243a2a58ef6acc3f2bcfee9b38f54d8331501d18
Mohammed Didarul Islam <55441986+Didarul1713@users.noreply.github.com>
2019-12-08 23:40:23 +0600

    Add files via upload

---
+++ Include/footer.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="container">
+        <div style="text-align: center;">
+            <div class="">
+                <h5>About Store</h5>
+                <img style="margin: 0 auto;" src="images/basic/logo-lite.png" class="img-responsive space10" alt=""/>
+                <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus</p>
+                <ul class="" style="text-align: center;">
+                    <li style="display: inline;"><a style="font-size: 150%; padding: 10px;;" href="https://www.facebook.com/" class="fa fa-facebook"></a></li>
+                    <li style="display: inline;"><a style="font-size: 150%; padding: 10px;;" href="https://www.twitter.com/" class="fa fa-twitter"></a></li>
+                    <li style="display: inline;"><a style="font-size: 150%; padding: 10px;;" href="https://feedburner.google.com/" class="fa fa-rss"></a></li>
+                    <li style="display: inline;"><a style="font-size: 150%; padding: 10px;;" href="mailto:email@website.com" class="fa fa-envelope"></a></li>
+                    <li style="display: inline;"><a style="font-size: 150%; padding: 10px;;" href="https://www.pinterest.com/" class="fa fa-pinterest"></a></li>
+                    <li style="display: inline;"><a style="font-size: 150%; padding: 10px;;" href="https://www.instagram.com/" class="fa fa-instagram"></a></li>
+                </ul>
+            </div>                    
+        </div>
+    </div>
+</footer>
